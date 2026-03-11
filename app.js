51c8a1b11ea6cbc9654100faf4dd43d79ed5ee00
ss.b.07 <sheetal87654321@gmail.com>
2026-03-11 22:04:28 +0530

    added render deployment fixes

--- app.js
+++ app.js
@@ -143,6 +143,8 @@ app.use((err,req,res,next)=>{
 });
 
 
-app.listen(8080,()=>{
-    console.log("Server is listening to port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
